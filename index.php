14d00d3ec9cdd0afe8251ae0b9153b35cf7ee1bb
yuuuri <yuuuri@yahoo.com>
2015-04-17 10:22:07 -0700

    applied bootstrap and updated as tp_index.php, will delete index.php later and made style.css to pretty up

--- index.php
+++ index.php
@@ -3,52 +3,73 @@
 ?>
 
 <html>
+<head>
+	<title>Term Project CS431</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link href="styles/style.css" rel="stylesheet" type="text/css">
+</head>
 <body>
 
-	
-
-	<b><font size = 16>CPSC 431 Term Project</font></b>
+	<h1><span class="label label-default">On-Campus Course Management System</span></h1>
+	<h5>CPSC 431 Term Project - by: Victor, Annette, Yuri</h5>
 	<br><br>
-	<b>Professor Login</b><br>
-	<form action="professor_page.php" method="POST">
-		Professor CWID: <input type="text" name="ssn" maxlength = "9"><br>
-		<input type="submit" value = "Login">
-	</form>
-	<?php
-		if(isset($_SESSION['message']))
-		{
-			echo '<font color = "red"><i>'.$_SESSION['message'].'</i></font>';
-		}
-		unset($_SESSION['message']); // clear the value so that it doesn't display again
-	?>
-	
 
-	<br>
-	<b>Student Login: </b><br>
-	<form action="student_page.php" method="POST">
-		Student CWID <input type="text" name="cwid" maxlength = "9"><br>
-		<input type="submit" value = "Login">
-	</form>
+	<div class="jumbotron">
+  	<h1>Student Login: </h1>
+		<form class="form-inline" form action="students.php" method="POST">
+		  <div class="form-group">
+		    <label for="exampleInputName2">Students ID: </label>
+		    <input type="text" class="form-control" id="exampleInputName2" placeholder="" name="cwid" maxlength = "9">
+		  	<button type="submit" class="btn btn-primary">Sign In</button>
+		  </div>
+		</form>
+	</div>
 	<?php
-		if(isset($_SESSION['message4']))
+		if(isset($_SESSION['message1']))
 		{
-			echo '<font color = "red"><i>'.$_SESSION['message4'].'</i></font>';
+			echo '<font color = "red"><i>'.$_SESSION['message1'].'</i></font>';
 		}
-		unset($_SESSION['message4']); // clear the value so that it doesn't display again
+		unset($_SESSION['message1']); // clear the value so that it doesn't display again
 	?>
 
 	<br>
-	<b>Admin Login: </b><br>
-	<form action="admin_page.php" method="POST">
-		Admin CWID <input type="text" name="cwid" maxlength = "9"><br>
-		<input type="submit" value = "Login">
-	</form>
+
+	<div class="jumbotron">
+  	<h1>Faculty Login: </h1>
+		<form class="form-inline" form action="students.php" method="POST">
+		  <div class="form-group">
+		    <label for="exampleInputName2">Faculty ID: </label>
+		    <input type="text" class="form-control" id="exampleInputName2" placeholder="" name="cwid" maxlength = "9">
+		  	<button type="submit" class="btn btn-primary">Sign In</button>
+		  </div>
+		</form>
+	</div>
 	<?php
-		if(isset($_SESSION['message4']))
+		if(isset($_SESSION['message2']))
 		{
-			echo '<font color = "red"><i>'.$_SESSION['message4'].'</i></font>';
+			echo '<font color = "red"><i>'.$_SESSION['message2'].'</i></font>';
 		}
-		unset($_SESSION['message4']); // clear the value so that it doesn't display again
+		unset($_SESSION['message2']); // clear the value so that it doesn't display again
+	?>
+
+	<br>
+
+	<div class="jumbotron">
+  	<h1>Administrator Stuff Login: </h1>
+		<form class="form-inline" form action="students.php" method="POST">
+		  <div class="form-group">
+		    <label for="exampleInputName2">Administrator ID: </label>
+		    <input type="text" class="form-control" id="exampleInputName2" placeholder="" name="cwid" maxlength = "9">
+		  	<button type="submit" class="btn btn-primary">Sign In</button>
+		  </div>
+		</form>
+	</div>
+	<?php
+		if(isset($_SESSION['message3']))
+		{
+			echo '<font color = "red"><i>'.$_SESSION['message3'].'</i></font>';
+		}
+		unset($_SESSION['message3']); // clear the value so that it doesn't display again
 	?>
 	
 	
