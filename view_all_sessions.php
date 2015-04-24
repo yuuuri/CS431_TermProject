d1bfe693743861f7eacec158f1331d5ea8ff2551
yuuuri <yuuuri@yahoo.com>
2015-04-24 12:11:05 -0700

    displays all sections but back button not working

---
+++ view_all_sessions.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>View All Sessions</title>
+    <meta name = "author" content="Yuri Van Steenburg" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link rel= "stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <header>
+        <div class = "view_all_sessions_header">
+        <h2> View All Sessions </h2>
+        </div>
+    </header>
+<main>
+    <div class = "all_sessions_tbl_div">
+        <table class = "table table-striped">
+            <thead>
+                <tr><th>Section ID</th>
+                    <th>Course ID</th>
+                    <th>Professor First Name</th>
+                    <th>Professor Last Name</th>
+                    <th>Meeting Date</th>
+                    <th>Starting Time</th>
+                    <th>End Time</th>
+                </tr>
+            </thead>
+            <tbody> <!-- Reference: https://github.com/chrisdanan/431Hw4/blob/master/index.php -->
+                <?php
+                    class view_all_sections {
+                        function connect() {
+                            //local variable to connect to database
+                            $user = 'root';
+                            $password = 'root';
+                            $db = 'TermProject';
+                            $host = '127.0.0.1';
+                            $port = 8889;
+                            $socket = 'localhost:/Applications/MAMP/tmp/mysql/mysql.sock';
+
+                            $link = mysqli_init();
+                            $success = mysqli_real_connect($link, $host, $user, $password, $db, $port, $socket);
+                            if (mysqli_connect_errno()) {
+                                echo "<p>Error: Could not connect to data base.  Try again<p>\n";
+                                exit;
+                            }
+                            return $link;
+                        }
+                        function display_all_sections($link_db){
+                            $select = 'SELECT Sec.Section_ID, Sec.Course_ID, P.Fname, P.Lname, Sec.Meeting_Date, Sec.Start_Time, Sec.End_Time ';
+                            $from = 'FROM SECTIONS as SEC, PROFESSOR as P ';
+                            $where = 'WHERE SEC.P_ID = P.P_ID;';
+                            $query = $select.$from.$where;
+                            $result = $link_db->query($query) or die("ERROR: " . mysqli_error($link_db));
+                            if($result->num_rows === 0){
+                                echo "<p>No records found</p>";
+                                //exit();
+                            }else {
+                                    while($row = mysqli_fetch_array($result)){
+                                        echo "<tr>";
+                                        echo "<td>" . $row['Section_ID'] . "</td>";
+                                        echo "<td>" . $row['Course_ID'] . "</td>";
+                                        echo "<td>" . $row['Fname'] . "</td>";
+                                        echo "<td>" . $row['Lname'] . "</td>";
+                                        echo "<td>" . $row['Meeting_Date'] . "</td>";
+                                        echo "<td>" . $row['Start_Time']."</td>";
+                                        echo "<td>" . $row['End_Time']."</td>";
+                                        echo "</tr>";
+                                    }//end of while
+
+                                $result->free();
+                                $link_db->close();
+
+                            }//end of else
+                        }//end of function display_all_sections
+                    }//end of class view_all_sections
+
+                    $v = new view_all_sections();
+                    $link_db = $v->connect();
+                    $v->display_all_sections($link_db);
+
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+</main>
+<footer>
+            <br>
+            <br>
+            <form action = 'student.php' method = 'LINK'>
+                <input type = 'submit' class = "btn btn-default" value = 'Back'>
+            </form>
+</footer>
+</body>
+</html>
