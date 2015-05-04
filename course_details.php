71f39e9fcb39fc5ca45422ab9723ca7b76a5b867
adjr17 <adjr17@csu.fullerton.edu>
2015-05-04 02:19:14 -0700

    Added to teaching_courses.php

--- course_details.php
+++ course_details.php
@@ -3,6 +3,16 @@
 	include 'define_class.php';
 	
 ?>
+
+/*<?php
+
+    $course_id = $_SESSION['course_id'];
+    $course_title = $_POST["Course_Title"];
+    $course_des = $_POST["Description"];
+   // $original_course_id = $_SESSION["course_id"];
+    
+?>*/
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -30,11 +40,45 @@
 			   <?php
 
                     $db = connectDB();
-					display_course($db, $course_id)
+					display_course($db, $course_id);
 
                 ?>
             </tbody>
         </table>
+    </div>
+    
+    <div class = "bottom_buttons" >
+                    <form class="form-inline" form action="edit_course_schedule.php" method="POST">
+                        <div class="form-group">
+                            <label for="exampleInputName2">Modify Details of a Course: </label>
+                            <input type="text" class="form-control" id="exampleInputName2" placeholder=" Enter Course ID" name="edit_course" maxlength = "8">
+                            <button type="submit" class="btn btn-info">Edit</button>
+                        </div>
+                    </form>
+                        <?php
+                            if(isset($_SESSION['message']))
+                            {
+                                echo '<font color = "red"><i>'.$_SESSION['message'].'</i></font>';
+                            }
+                            unset($_SESSION['message']); // clear the value so that it doesn't display again
+                        ?>
+    </div>
+    <br /> <br />
+    
+    <div class = "bottom_buttons" >
+                    <form class="form-inline" form action="modify_course_schedule.php" method="POST">
+                        <div class="form-group">
+
+                            <button type="submit" class="btn btn-info">Add/Delete a Course</button>
+                        </div>
+                    </form>
+                        <?php
+                            if(isset($_SESSION['message_c']))
+                            {
+                                echo '<font color = "red"><i>'.$_SESSION['message_c'].'</i></font>';
+                            }
+                            unset($_SESSION['message_c']); // clear the value so that it doesn't display again
+                        ?>
     </div>
 
 	<br /> 
