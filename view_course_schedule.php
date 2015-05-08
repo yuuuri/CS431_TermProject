7aa7ebe542affd830a3a04b6c6e1b7c0cd4a7724
yuuuri <yuuuri@yahoo.com>
2015-05-08 13:49:12 -0700

    The bottom has 'back' button instead 'submit'

--- view_course_schedule.php
+++ view_course_schedule.php
@@ -19,88 +19,69 @@
         </div>
     </header>
 <main>
-    <div class = "Course_Schedule_tbl_div">
+    <div class = "my_classes_tbl_div">
         <table class = "table table-striped">
             <thead>
                 <tr><th>Course ID</th>
                     <th>Course Title    </th>
 					<th>Course Description</th>
-					<th>Course Units </th>
                 </tr>
             </thead>
             <tbody> <!-- Reference: https://github.com/chrisdanan/431Hw4/blob/master/index.php -->
                
 			   <?php
-
                     $db = connectDB();
 					display_course_schedule($db);
-
                 ?>
             </tbody>
         </table>
     </div>
-	
-	<br><br>
-	
     <div class = "bottom_buttons" >
                     <form class="form-inline" form action="edit_course_schedule.php" method="POST">
                         <div class="form-group">
-                            <label for="exampleInputName2">Modify Details of a Course </label>
+                            <label for="exampleInputName2">Modify Details of a Course: </label>
                             <input type="text" class="form-control" id="exampleInputName2" placeholder=" Enter Course ID" name="edit_course" maxlength = "8">
                             <button type="submit" class="btn btn-info">Edit</button>
                         </div>
                     </form>
                         <?php
-                            if(isset($_SESSION['message_edit_course']))
+                            if(isset($_SESSION['message']))
                             {
-                                echo '<font color = "red"><i>'.$_SESSION['message_edit_course'].'</i></font>';
+                                echo '<font color = "red"><i>'.$_SESSION['message'].'</i></font>';
                             }
-                            unset($_SESSION['message_edit_course']); // clear the value so that it doesn't display again
+                            unset($_SESSION['message']); // clear the value so that it doesn't display again
                         ?>
     </div>
 	<br /> <br />
 	
-    <div class = "bottom_buttons" >
-                    <form class="form-inline" form action="delete_course.php" method="POST">
+	<div class = "bottom_buttons" >
+                    <form class="form-inline" form action="modify_course_schedule.php" method="POST">
                         <div class="form-group">
-                            <label for="exampleInputName2">Delete Course </label>
-                            <input type="text" class="form-control" id="exampleInputName2" placeholder=" Enter Course ID" name="del_course" maxlength = "8">
-                            <button type="submit" class="btn btn-info">Submit</button>
+
+                            <button type="submit" class="btn btn-info">Add/Delete a Course</button>
                         </div>
                     </form>
                         <?php
-                            if(isset($_SESSION['message_del_course']))
+                            if(isset($_SESSION['message_c']))
                             {
-                                echo '<font color = "red"><i>'.$_SESSION['message_del_course'].'</i></font>';
+                                echo '<font color = "red"><i>'.$_SESSION['message_c'].'</i></font>';
                             }
-                            unset($_SESSION['message_del_course']); // clear the value so that it doesn't display again
+                            unset($_SESSION['message_c']); // clear the value so that it doesn't display again
                         ?>
     </div>
 	
-	<br> <br>
-	
-	<div class = "bottom_buttons" >
-                    <form class="form-inline" form action="add_course.php" method="POST">
-                        <div class="form-group">
-
-                            <button type="submit" class="btn btn-info">Add Course</button>
-                        </div>
-                    </form>
-
-    </div>
-	
 </main>
-</body>
 <footer>
-            <br>
-            <br>
-			<form action = "admin.php" method = "post">
-				<input name = "BackButton" type= "submit" value= "Back">
-			</form>
-
-
-
-
+        <br>
+        <br>
+        <div class = "bottom_buttons" >
+            <form action = 'admin.php' method = 'POST'>
+                <input type = 'submit' class = "btn btn-default" value = 'Back'>
+            </form>
+        </div>
 </footer>
+</body>
+</html>
+
 </body>
 </html>
