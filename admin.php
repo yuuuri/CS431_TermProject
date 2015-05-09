09dc012c90daf1b35bce4671dea0e01140765c6a
victorwei <wei.unit@gmail.com>
2015-05-08 22:20:59 -0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -3,7 +3,6 @@
 	include 'define_class.php';
 ?>
 
-
 <?php
 
 	$accounttype = 'ADMIN_STAFF';
@@ -27,17 +26,16 @@
 		if (!$admin_ID){
 			$_SESSION['message3'] = "Please enter your Administrator ID";
 			header("Location: index.php");	
-		} elseif(!checkCWID($admin_ID,$accounttype)) {	
-			$_SESSION['message3'] = "ID entered not valid!";
-			header("Location: index.php");
 		} elseif(strlen($admin_ID) < 9) {
 			$_SESSION['message3'] = "SSN must be 9 digits";
 			header("Location: index.php");
 		} elseif(!is_numeric($admin_ID)) {
 			$_SESSION['message3'] = "Please enter numeric";
 			header("Location: index.php");
+		} elseif(!checkCWID($admin_ID,$accounttype)) {	
+			$_SESSION['message3'] = "ID entered not valid!";
+			header("Location: index.php");
 		}
-	
 		$admin_fname = getFname($admin_ID, $accounttype);
 		$admin_lname = getLname($admin_ID, $accounttype);
 		$_SESSION["fname"] = $admin_fname;
@@ -90,10 +88,8 @@
         </form>
     	<br><br><br><br><br>
     	
-		<form action = "index.php" method = "post">
-            <input class = "btn btn-primary btn-lg" type = "submit" value = "LOG OUT"/>
-            <?php session_destroy() ?>
-        </form>
+
+        <a href='logout.php'><h4>Click here to log out</h4></a>
     
 
     </div>
