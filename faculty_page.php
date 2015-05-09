2887a38cf47039f3408c90f03d52c6eca0ed111f
adjr615 <adjr17@csu.fullerton.edu>
2015-05-08 22:10:49 -0700

    Update faculty_page.php
    Fixed connection issue

--- faculty_page.php
+++ faculty_page.php
@@ -1,10 +1,14 @@
 <?php
-    session_start();
-    $facultyId = $_POST['p_id'];
-    include 'define_class.php';
-    $_SESSION['id'] = $_POST['p_id'];
-    $account = 'PROFESSOR';
+     session_start();
+     $facultyId = $_POST['p_id'];
+     include 'define_class.php';
+     $_SESSION['id'] = $_POST['p_id'];
+     $account = 'PROFESSOR';
 ?>
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,7 +20,7 @@
 <body>
 <header>
     <div class = "student_greeting">
-        <h2> Faculty Page </h2>
+        <h2>Faculty Page</h2>
     </div>
 </header>
 
@@ -40,55 +44,14 @@
     else{
     $facultyFname = getFname($facultyId, $account);
     $facultyLname = getLname($facultyId, $account);
-    
-        //local variable to connect to database
-        /*$user = 'root';
-        $password = 'root';
-        $db = 'TermProject';
-        $host = '127.0.0.1';
-        $port = 8889;
-        $socket = 'localhost:/Applications/MAMP/tmp/mysql/mysql.sock';
-
-        $mysql_hostname = '127.0.0.1';
-        $mysql_username = 'root';
-        $mysql_password = 'root';
-        $mysql_database = 'TermProject'
-        //$db = mysql_connect($mysql_hostname, $mysql_username, $mysql_password);
-        
-        $link = mysqli_init();
-        $db = mysqli_real_connect($link, $mysql_hostname, $mysql_username, $mysql_password);
-        if (mysqli_connect_errno()) {
-            echo "<p>Error: Could not connect to database.  Try again<p>\n";
-            exit;
-        } 
-        //echo "Successfully connected to database TermProject\n";
-        //database connected
-
-        $query = 'SELECT P_ID, Fname, Lname from PROFESSOR WHERE P_ID = '.$facultyId.';';
-        $result = $link->query($query) or die("ERROR: " . mysqli_error($link));
-        if($result->num_rows === 0){
-            $_SESSION['message1'] = "Please enter valid faculty ID.";
-            header("Location: index.php");
-        }else{
-
-            //create a session variable
-            $_SESSION['sess_var'] = $facultyId;
-
-            //echo 'The content of $_SESSION[\'session_var\'] is'.$_SESSION['sess_var'].'<br />';
-
-            $row = $result->fetch_assoc();
-            $student_id = $row['P_ID'];
-            $student_fname = $row['Fname'];
-            $student_lname = $row['Lname'];
-            $link->close();
-        }
-    */}
+    }
 ?>
 
 <main>
     <div class = "s">
         <?php
-            echo '<h3 >Hello '.$facultyId.'!</h3>';
+            echo '<h3>Welcome '.$facultyFname.' '.$facultyLname.'</h3>';
+            echo '<h3>Faculty ID:' .$facultyId.'</h3>';
         ?>
 
     </div>
@@ -97,7 +60,7 @@
     <br>
     <div class = "coursesList">
         <form action = "teaching_courses.php" method = "post">
-            <input class = "btn btn-primary btn-lg" type = "submit" value = "View Courses"/>
+            <input class = "btn btn-primary btn-lg" type = "submit" name = 'p_id' value = "View Courses"/>
         </form>
     </div>
         <br>
@@ -105,7 +68,7 @@
         <br>
     <div class = "uploadMaterials">
         <form action = "" method = "post">
-            <input class="btn btn-primary btn-lg" type = "submit" name = 's_id' value = "Upload Files" />
+            <input class="btn btn-primary btn-lg" type = "submit" name = 'p_id' value = "Upload Files" />
         </form>
     </div>
         <br>
@@ -113,21 +76,24 @@
         <br>
     <div class = "downloadFiles">
         <form action = "" method = "post">
-            <input class="btn btn-primary btn-lg" type = "submit" name = 's_id' value = "Download Files" />
+            <input class="btn btn-primary btn-lg" type = "submit" name = 'p_id' value = "Download Files" />
         </form>
     </div>
         <br>
         <br>
         <br>
-    <div class = "enterScores">
-        <form action = "" method = "post">
-            <input class="btn btn-primary btn-lg" type = "submit" name = 's_id' value = "Enter Scores" />
+<!--     <div class = "enterScores">
+        <form action = "enter_scores.php" method = "post">
+            <input class="btn btn-primary btn-lg" type = "submit" name = 'section_id' value = "Enter Scores" />
         </form>
-    </div>
+    </div> -->
 
 </main>
 
 </body>
 </html>
+
+       
+
 
        
