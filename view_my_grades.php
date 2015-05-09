decdccc489d72c7e37364fe095a03da8e2bb2be6
yuuuri <yuuuri@yahoo.com>
2015-05-08 21:36:43 -0700

    same style as Victor has

--- view_my_grades.php
+++ view_my_grades.php
@@ -1,7 +1,39 @@
 <?php
     session_start();
-    $student_id = $_SESSION['sess_var'];
+    include 'define_class.php';
+    $student_id = $_SESSION['id'];
+
+            function display_my_grades($link_db, $student_id){
+
+                    $select = 'SELECT CG.Section_ID, SE.Course_ID, CG.HW_Grade, CG.Term_Grade, CG.Course_Grade ';
+                    $from = 'FROM CLASS_GRADES as CG, SECTIONS as SE ';
+                    $where = 'WHERE CG.S_ID = '.$student_id.' and CG.Section_ID = SE.Section_ID;';
+                    $query = $select.$from.$where;
+                    //$querty = 'SELECT Section_ID FROM CLASS_GRADES;';
+
+                    $result = $link_db->query($query) or die("ERROR: " . mysqli_error($link_db));
+                    if($result->num_rows === 0){
+                        echo "<p>No records found</p>";
+                        //exit();
+                    }else {
+                            while($row = mysqli_fetch_array($result)){
+                                echo "<tr>";
+                                echo "<td>" . $row['Section_ID'] . "</td>";
+                                echo "<td>" . $row['Course_ID'] ."</td>";
+                                echo "<td>" . $row['HW_Grade'] . "</td>";
+                                echo "<td>" . $row['Term_Grade'] . "</td>";
+                                echo "<td>" . $row['Course_Grade']."</td>";
+                                echo "</tr>";
+                            }//end of while
+
+                        //$result->free();
+                        //$link_db->close();
+
+                    }//end of else
+            }//end of function display_my_grades
 ?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -33,57 +65,9 @@
                     //echo 'The content of $_SESSION[\'session_var\'] is'.$_SESSION['sess_var'].'<br />';
                     //echo '$student_id is '.$student_id;
 
-                    class view_my_grades {
-                        function connect() {
-                            //local variable to connect to database
-                            $user = 'root';
-                            $password = 'root';
-                            $db = 'TermProject';
-                            $host = '127.0.0.1';
-                            $port = 8889;
-                            $socket = 'localhost:/Applications/MAMP/tmp/mysql/mysql.sock';
-
-                            $link = mysqli_init();
-                            $success = mysqli_real_connect($link, $host, $user, $password, $db, $port, $socket);
-                            if (mysqli_connect_errno()) {
-                                echo "<p>Error: Could not connect to data base.  Try again<p>\n";
-                                exit;
-                            }
-                            return $link;
-                        }
-                        function display_my_grades($link_db){
-
-                            $select = 'SELECT CG.Section_ID, SE.Course_ID, CG.HW_Grade, CG.Term_Grade, CG.Course_Grade ';
-                            $from = 'FROM CLASS_GRADES as CG, SECTIONS as SE ';
-                            $where = 'WHERE CG.S_ID = '.$_SESSION['sess_var'].' and CG.Section_ID = SE.Section_ID;';
-                            $query = $select.$from.$where;
-                            //$querty = 'SELECT Section_ID FROM CLASS_GRADES;';
-
-                            $result = $link_db->query($query) or die("ERROR: " . mysqli_error($link_db));
-                            if($result->num_rows === 0){
-                                echo "<p>No records found</p>";
-                                //exit();
-                            }else {
-                                    while($row = mysqli_fetch_array($result)){
-                                        echo "<tr>";
-                                        echo "<td>" . $row['Section_ID'] . "</td>";
-                                        echo "<td>" . $row['Course_ID'] ."</td>";
-                                        echo "<td>" . $row['HW_Grade'] . "</td>";
-                                        echo "<td>" . $row['Term_Grade'] . "</td>";
-                                        echo "<td>" . $row['Course_Grade']."</td>";
-                                        echo "</tr>";
-                                    }//end of while
-
-                                $result->free();
-                                $link_db->close();
-
-                            }//end of else
-                        }//end of function display_all_sections
-                    }//end of class view_all_sections
-
-                    $v = new view_my_grades();
-                    $link_db = $v->connect();
-                    $v->display_my_grades($link_db);
+                    //local variable to connect to database    
+                    $db = connectDB();
+                    display_my_grades($db, $student_id);
 
                 ?>
             </tbody>
