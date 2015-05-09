819df5e6479e47cf917bb48f48431849ab4a8c7d
yuuuri <yuuuri@yahoo.com>
2015-05-09 02:40:21 -0700

    stops if the user enter the class to go if user is not in that class

--- view_a_class.php
+++ view_a_class.php
@@ -1,56 +1,83 @@
 <?php
     session_start();
     include 'define_class.php';
-    $student_id = $_SESSION['id'];
+    $student_id = $_SESSION['id'];    
+    //declare course(specific section) variable
+    $sec_num = $_POST['asec_num']; //asec_num is a section number picked up from view_my_classes
     
-    
-                function view_specific_class($link, $student_id, $sec_num) {
-                    
-                    $select = 'SELECT s.Section_ID, s.Course_ID, s.Meeting_Date, s.Start_Time, s.End_Time, c.Course_Unit, s.Syllabus ';
-                    $from = 'FROM COURSE as c, SECTIONS as s, ENROLL as e ';
-                    $where = 'WHERE e.S_ID = '.$student_id.' AND e.Section_ID = s.Section_ID AND s.Course_ID = c.Course_ID;';
-                    $query = $select.$from.$where;
-                    $result = $link->query($query) or die("ERROR: " . mysqli_error($link));
-                    if($result->num_rows === 0){
-                        echo "<p>No records found</p>";
+    if(!$sec_num){
+        $_SESSION['message_aclass'] = "Please enter Section Number.";
+        header("Location: view_my_classes.php");
+    }elseif(strlen($sec_num) > 3){
+        $_SESSION['message_aclass'] = "Invalid Section ID, Please enter Section ID above.";
+        header("Location: view_my_classes.php");
+    }elseif(!is_numeric($sec_num)){
+        $_SESSION['message_aclass'] = "Please enter numeric digits.";
+        header("Location: view_my_classes.php");
+    }else{
+        $link = connectDB();
+        $select = 'SELECT s.Section_ID, s.Course_ID, s.Meeting_Date, s.Start_Time, s.End_Time, c.Course_Unit, s.Syllabus ';
+        $from = 'FROM COURSE as c, SECTIONS as s, ENROLL as e ';
+        $where = 'WHERE e.S_ID = '.$student_id.' AND e.Section_ID = s.Section_ID AND s.Course_ID = c.Course_ID;';
+        $query = $select.$from.$where;
+        $result = $link->query($query) or die("ERROR: " . mysqli_error($link));
+        if($result->num_rows === 0){
+            $_SESSION['message_aclass'] = "No Record Found.";
+        header("Location: view_my_classes.php");
+        }else{
+            //echo 'variable check $sec_num = '.$sec_num;
+            //echo 'variable check $student_id var = '.$student_id;
+        $query_one_class = 'SELECT s.Section_ID, s.Course_ID, c.Course_Title, p.Lname, s.Meeting_Date, s.Start_Time, s.End_Time, c.Course_Unit, s.Syllabus FROM SECTIONS as s, COURSE as c, PROFESSOR as p, ENROLL as e WHERE e.S_ID = '.$student_id.' and '.$sec_num.' = e.Section_ID and e.Section_ID = s.Section_ID and s.Course_ID = c.Course_ID and s.P_ID = p.P_ID';
+        $result_aclass = $link->query($query_one_class) or die("ERROR:" . mysqli_error($link));                     
+            if($result_aclass->num_rows === 0){
+                $_SESSION['message_aclass'] = "No records found, only one class should be displayed here.";
+                header("Location: view_my_classes.php");
+            }
+        }
+    }
+
+        function view_specific_class($link, $student_id, $sec_num) {
+                
+                $select = 'SELECT s.Section_ID, s.Course_ID, s.Meeting_Date, s.Start_Time, s.End_Time, c.Course_Unit, s.Syllabus ';
+                $from = 'FROM COURSE as c, SECTIONS as s, ENROLL as e ';
+                $where = 'WHERE e.S_ID = '.$student_id.' AND e.Section_ID = s.Section_ID AND s.Course_ID = c.Course_ID;';
+                $query = $select.$from.$where;
+                $result = $link->query($query) or die("ERROR: " . mysqli_error($link));
+                if($result->num_rows === 0){
+                    echo "<p>No records found</p>";
+                    exit();
+                }else{
+                    //echo 'variable check $sec_num = '.$sec_num;
+                    //echo 'variable check $student_id var = '.$student_id;
+                $query_one_class = 'SELECT s.Section_ID, s.Course_ID, c.Course_Title, p.Lname, s.Meeting_Date, s.Start_Time, s.End_Time, c.Course_Unit, s.Syllabus FROM SECTIONS as s, COURSE as c, PROFESSOR as p, ENROLL as e WHERE e.S_ID = '.$student_id.' and '.$sec_num.' = e.Section_ID and e.Section_ID = s.Section_ID and s.Course_ID = c.Course_ID and s.P_ID = p.P_ID';
+                $result_aclass = $link->query($query_one_class) or die("ERROR:" . mysqli_error($link));                     
+                    if($result_aclass->num_rows === 0){
+                        echo "<p>No records found, only one class should be displayed here</p>";
                         exit();
                     }else{
-                        //echo 'variable check $sec_num = '.$sec_num;
-                        //echo 'variable check $student_id var = '.$student_id;
-                    $query_one_class = 'SELECT s.Section_ID, s.Course_ID, c.Course_Title, p.Lname, s.Meeting_Date, s.Start_Time, s.End_Time, c.Course_Unit, s.Syllabus FROM SECTIONS as s, COURSE as c, PROFESSOR as p, ENROLL as e WHERE e.S_ID = '.$student_id.' and '.$sec_num.' = e.Section_ID and e.Section_ID = s.Section_ID and s.Course_ID = c.Course_ID and s.P_ID = p.P_ID';
-                    $result_aclass = $link->query($query_one_class) or die("ERROR:" . mysqli_error($link));                     
-                        if($result_aclass->num_rows === 0){
-                            echo "<p>No records found, only one class should be displayed here</p>";
-                            exit();
-                        }else{
-                            //create another session variable
-                            $_SESSION['hw_sec_var'] = $sec_num;
-                            //echo $_SESSION['hw_sec_var'];
-                            while($row = mysqli_fetch_array($result_aclass)){
-                                    echo "<tr>";
-                                    echo "<td>" . $row['Section_ID'] . "</td>";
-                                    echo "<td>" . $row['Course_ID'] . "</td>";
-                                    echo "<td>" . $row['Course_Title'] . "</td>";
-                                    echo "<td>" . $row['Lname'] . "</td>";
-                                    echo "<td>" . $row['Meeting_Date'] . "</td>";
-                                    echo "<td>" . $row['Start_Time']."</td>";
-                                    echo "<td>" . $row['End_Time']."</td>";
-                                    echo "<td>" . $row['Course_Unit']."</td>";
-                                    echo "<td>" . $row['Syllabus']."</td>";
-                                    echo "</tr>";
-                            }//end of while
-                            //$result_aclass->$free();
-                            //$link->close();
-                            //echo 'what happened2?'; 
-                        } 
-                        
-                        
-                              
-                    }
+                        //create another session variable
+                        $_SESSION['hw_sec_var'] = $sec_num;
+                        //echo $_SESSION['hw_sec_var'];
+                        while($row = mysqli_fetch_array($result_aclass)){
+                                echo "<tr>";
+                                echo "<td>" . $row['Section_ID'] . "</td>";
+                                echo "<td>" . $row['Course_ID'] . "</td>";
+                                echo "<td>" . $row['Course_Title'] . "</td>";
+                                echo "<td>" . $row['Lname'] . "</td>";
+                                echo "<td>" . $row['Meeting_Date'] . "</td>";
+                                echo "<td>" . $row['Start_Time']."</td>";
+                                echo "<td>" . $row['End_Time']."</td>";
+                                echo "<td>" . $row['Course_Unit']."</td>";
+                                echo "<td>" . $row['Syllabus']."</td>";
+                                echo "</tr>";
+                        }//end of while
+                        //$result_aclass->$free();
+                        //$link->close();
+                        //echo 'what happened2?'; 
+                    }       
                 }
-    
-    
-    
+            
+        }//end of view_specific_class    
     
 ?>
 <!DOCTYPE html>
@@ -86,18 +113,7 @@
             <tbody> <!-- Reference: https://github.com/chrisdanan/431Hw4/blob/master/index.php -->
 
             <?php
-                //declare course(specific section) variable
-                $sec_num = $_POST['asec_num']; //asec_num is a section number picked up from view_my_classes
-                if(!$sec_num){
-                    $_SESSION['message_aclass'] = "Please enter Section Number.";
-                    header("Location: view_my_classes.php");
-                }elseif(strlen($sec_num) > 3){
-                    $_SESSION['message_aclass'] = "Invalid Section ID, Please enter Section ID above.";
-                    header("Location: view_my_classes.php");
-                }elseif(!is_numeric($sec_num)){
-                    $_SESSION['message_aclass'] = "Please enter numeric digits.";
-                    header("Location: view_my_classes.php");
-                }else{ //echo 'course_id is '.$sec_num;
+                 //echo 'course_id is '.$sec_num;
                     
                     //local variable to connect to database
                     $db = connectDB();
@@ -105,7 +121,7 @@
                     
                 //$result->$free();
                 //$link->close();    
-               }
+               
             ?>  
             </tbody>
         </table>
@@ -114,7 +130,7 @@
 <br><br>
     <div class = "bottom_buttons" >          
         <h3>Submit My Homework</h3>
-            <form action = "upload_hw.php" method = "post" enctype="multipart/form-data" />
+            <form action = "add_file.php" method = "post" enctype="multipart/form-data" />
                 <div>
                     <input type = "hidden" name = "MAX_FILE_SIZE" value = "1000000" />
                     <label for ="userfile"><h4>Upload a file: </h4></label>
